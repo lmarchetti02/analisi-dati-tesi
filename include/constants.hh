af14afb1429482fbc8b3245b0305a9517f435bbc
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-06 17:04:23 +0100

    removed #define

--- include/constants.hh
+++ include/constants.hh
@@ -1,7 +1,7 @@
 #pragma once
 
-#define INFO_COLOR "\033[38;5;51m"
-#define ERROR_COLOR "\033[38;5;160m"
-#define DEBUG_COLOR "\033[38;5;35m"
-#define END_COLOR "\033[0m"
-#define BOLD "\033[1m"
+constexpr const char INFO_COLOR[] = "\033[38;5;51m";
+constexpr const char ERROR_COLOR[] = "\033[38;5;160m";
+constexpr const char DEBUG_COLOR[] = "\033[38;5;35m";
+constexpr const char END_COLOR[] = "\033[0m";
+constexpr const char BOLD[] = "\033[1m";
