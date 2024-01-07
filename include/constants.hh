8ec9f9e84c63b60e48f0cfcede0e30e1f41e3a2f
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-07 13:24:00 +0100

    boh

--- include/constants.hh
+++ include/constants.hh
@@ -5,3 +5,8 @@ constexpr const char ERROR_COLOR[] = "\033[38;5;160m";
 constexpr const char DEBUG_COLOR[] = "\033[38;5;35m";
 constexpr const char END_COLOR[] = "\033[0m";
 constexpr const char BOLD[] = "\033[1m";
+
+constexpr const char filename_def[] = "output0.root";
+constexpr int n_thresholds_def = 0;
+constexpr double min_threshold_def = 0.;
+constexpr double max_threshold_def = 80.;
---
+++ src/options.cpp
@@ -0,0 +1,15 @@
+#include "options.hh"
+
+#include <filesystem>
+
+#include "constants.hh"
+
+const std::filesystem::path options_path{"../utils/options.txt"};
+
+Options::Options()
+{
+}
+
+Options::~Options()
+{
+}
